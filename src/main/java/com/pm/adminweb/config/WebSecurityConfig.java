bce47282f2d834bd736cd1679e5d1feb1726d0ac
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-03-06 07:06:37 +0500

    Add login page

--- src/main/java/com/pm/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/pm/adminweb/config/WebSecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
